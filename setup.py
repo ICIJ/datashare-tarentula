8c5a814cfd64df49f34f610984aded09704a2627
Pierre Romera <hello@pirhoo.com>
2019-10-24 19:15:08 +0200

    Bump version: 0.5.2 → 0.6.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tarentula',
-    version='0.5.2',
+    version='0.6.0',
     packages=find_packages(),
     test_suite='nose.collector',
     tests_require=['nose', 'responses'],

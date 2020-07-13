8ddea3562ea368340f1d87cb6da7b821609798de
Pierre Romera <hello@pirhoo.com>
2020-07-13 14:33:40 +0200

    Bump version: 1.3.2 → 1.4.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tarentula',
-    version='1.3.2',
+    version='1.4.0',
     packages=find_packages(),
     test_suite='nose.collector',
     tests_require=['nose', 'responses'],

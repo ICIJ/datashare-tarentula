6d0350dba113d2102571fff15979b165bfbb2764
Pierre Romera <hello@pirhoo.com>
2020-07-17 13:43:11 +0200

    test: remove ./tmp directory after each test and fix doc ids

--- tests/test_download.py
+++ tests/test_download.py
@@ -45,37 +45,39 @@ class TestDownload(TestCase):
         with self.existing_species_documents() as species:
             runner = CliRunner()
             result = runner.invoke(cli, ['download', '--datashare-url', self.datashare_url, '--datashare-project', self.datashare_project, '--no-raw-file', '--query', 'name:*'])
-            self.assertIn('Downloading 10 document(s)', result.output)
+            self.assertIn('Downloading 20 document(s)', result.output)
 
     def test_summary_with_scroll(self):
         with self.existing_species_documents() as species:
             runner = CliRunner()
             result = runner.invoke(cli, ['download', '--datashare-url', self.datashare_url, '--datashare-project', self.datashare_project, '--no-raw-file', '--query', 'name:*', '--scroll', '1m'])
-            self.assertIn('Downloading 10 document(s)', result.output)
+            self.assertIn('Downloading 20 document(s)', result.output)
 
     def test_summary_with_wildcard(self):
         with self.existing_species_documents() as species:
             runner = CliRunner()
             result = runner.invoke(cli, ['download', '--datashare-url', self.datashare_url, '--datashare-project', self.datashare_project, '--no-raw-file', '--query', 'name:*dae'])
-            self.assertIn('Downloading 10 document(s)', result.output)
+            self.assertIn('Downloading 20 document(s)', result.output)
 
     def test_summary_with_wildcard_sta(self):
         with self.existing_species_documents() as species:
             runner = CliRunner()
             result = runner.invoke(cli, ['download', '--datashare-url', self.datashare_url, '--datashare-project', self.datashare_project, '--no-raw-file', '--query', 'name:*dae'])
-            self.assertIn('Downloading 10 document(s)', result.output)
+            self.assertIn('Downloading 20 document(s)', result.output)
 
     def test_meta_is_downloaded_for_actinopodidae(self):
         with self.existing_species_documents() as species:
             runner = CliRunner()
             runner.invoke(cli, ['download', '--datashare-url', self.datashare_url, '--datashare-project', self.datashare_project, '--no-raw-file', '--query', 'name:Actinopodidae'])
-            self.assertTrue(os.path.isfile(root('tmp/l7/Vn/l7VnZZEzg2fr960NWWEG.json')))
+            json = loadJsonFile('tmp/l7/Vn/l7VnZZEzg2fr960NWWEG.json')
+            self.assertEqual(json['_id'], 'l7VnZZEzg2fr960NWWEG')
 
     def test_meta_is_downloaded_for_ctenizidae(self):
         with self.existing_species_documents() as species:
             runner = CliRunner()
             runner.invoke(cli, ['download', '--datashare-url', self.datashare_url, '--datashare-project', self.datashare_project, '--no-raw-file', '--query', 'name:Ctenizidae'])
-            self.assertTrue(os.path.isfile(root('tmp/Bm/ov/BmovvXBisWtyyx6o9cuG.json')))
+            json = loadJsonFile('tmp/Bm/ov/BmovvXBisWtyyx6o9cuG.json')
+            self.assertEqual(json['_id'], 'BmovvXBisWtyyx6o9cuG')
 
     def test_meta_is_downloaded_for_idiopidae_with_default_properties(self):
         with self.existing_species_documents() as species:
@@ -90,7 +92,7 @@ class TestDownload(TestCase):
         with self.existing_species_documents() as species:
             runner = CliRunner()
             runner.invoke(cli, ['download', '--datashare-url', self.datashare_url, '--datashare-project', self.datashare_project, '--no-raw-file', '--query', 'name:Idiopidae', '--source', 'name'])
-            json = loadJsonFile('tmp/DW/LO/DWLOskax28jPQ2CjFrCo.json')
+            json = loadJsonFile('tmp/Dz/LO/DzLOskax28jPQ2CjFrCo.json')
             self.assertIn('_id', json)
             self.assertIn('_source', json)
             self.assertIn('name', json['_source'])

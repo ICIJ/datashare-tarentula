0fd2d0d704c118093b765a2885f9747f47117dbf
Pierre Romera <hello@pirhoo.com>
2020-12-01 11:36:29 +0100

    Bump version: 2.2.0 → 2.3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='tarentula',
-      version='2.2.0',
+      version='2.3.0',
       packages=find_packages(),
       description="Cli toolbelt for Datashare.",
       long_description=long_description,

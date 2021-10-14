322c85094a3ca54095f42b4eda0d4c9db48b8e8a
Miguel <miguel.fiandor.gutierrez@gmail.com>
2021-10-14 13:17:33 +0200

    fix: tests for Coun command

--- tests/test_count.py
+++ tests/test_count.py
@@ -11,16 +11,34 @@ class TestCount(TestAbstract):
     def tearDown(self):
         super().tearDown()
 
+    @staticmethod
+    def extract_num_docs_from_response(message):
+        return message.rsplit(" ", 1)[1].strip()
+
+    def test_count_all(self):
+        with self.existing_species_documents():
+            runner = CliRunner()
+            result = runner.invoke(cli, ['count', '--datashare-url', self.datashare_url, '--elasticsearch-url',
+                                self.elasticsearch_url, '--datashare-project', self.datashare_project])
+            self.assertEqual('20', self.extract_num_docs_from_response(result.output))
+
+            result = runner.invoke(cli, ['count', '--datashare-url', self.datashare_url, '--elasticsearch-url',
+                                self.elasticsearch_url, '--datashare-project', self.datashare_project, '--query',
+                                '*'])
+            self.assertEqual('20', self.extract_num_docs_from_response(result.output))
+
     def test_count_docs_1(self):
-        runner = CliRunner()
-        result = runner.invoke(cli, ['count', '--datashare-url', self.datashare_url, '--elasticsearch-url',
-                            self.elasticsearch_url, '--datashare-project', self.datashare_project, '--query',
-                            'Actinopodidae OR Antrodiaetidae'])
-        self.assertEqual('2', result.output)
+        with self.existing_species_documents():
+            runner = CliRunner()
+            result = runner.invoke(cli, ['count', '--datashare-url', self.datashare_url, '--elasticsearch-url',
+                                self.elasticsearch_url, '--datashare-project', self.datashare_project, '--query',
+                                'Actinopodidae OR Antrodiaetidae'])
+            self.assertEqual('2', self.extract_num_docs_from_response(result.output))
 
     def test_count_docs_2(self):
-        runner = CliRunner()
-        result = runner.invoke(cli, ['count', '--datashare-url', self.datashare_url, '--elasticsearch-url',
-                            self.elasticsearch_url, '--datashare-project', self.datashare_project, '--query',
-                            '_id:6VEdcVlWszkUd94XeuSd'])
-        self.assertEqual('1', result.output)
+        with self.existing_species_documents():
+            runner = CliRunner()
+            result = runner.invoke(cli, ['count', '--datashare-url', self.datashare_url, '--elasticsearch-url',
+                                self.elasticsearch_url, '--datashare-project', self.datashare_project, '--query',
+                                '_id:6VEdcVlWszkUd94XeuSd'])
+            self.assertEqual('1', self.extract_num_docs_from_response(result.output))

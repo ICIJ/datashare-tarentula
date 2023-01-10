7a7348291707432906500463346d8d8c969f2657
Bruno Thomas <bthomas@icij.org>
2023-01-10 16:56:53 +0000

    feat: add metadata fields basic command
    Co-authored-by: Miguel Fiandor <miguelfg@users.noreply.github.com>

---
+++ tests/test_metadata_fields.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from json import loads
+
+from click.testing import CliRunner
+
+from tarentula.cli import cli
+from tests.test_abstract import TestAbstract
+
+
+class TestMetadataFields(TestAbstract):
+    def tearDown(self):
+        self.datashare_client.delete_all(index=self.datashare_project)
+
+    def test_basic_metadata_field_list(self):
+        self.index_documents([{"name": "Antrodiaetidae", "type": "Document", "contentType": "audio/vnd.wave",
+                               "_id": "id1"}
+                              ])
+        runner = CliRunner()
+        result = runner.invoke(cli, ['list-metadata', '--datashare-url', self.datashare_url, '--elasticsearch-url',
+                                     self.elasticsearch_url, '--datashare-project', self.datashare_project])
+        json_result = loads(result.output)
+        self.assertEqual([
+            {"field": "contentType", "type": "keyword", "count": 1},
+            {"field": "extractionDate", "type": "date", "count": 1},
+            {"field": "name", "type": "text", "count": 1},
+            {"field": "type", "type": "keyword", "count": 1}], json_result)
+
+    def test_basic_metadata_field_list_with_tika_metadata(self):
+        self.index_documents([{"name": "Antrodiaetidae", "type": "Document", "contentType": "audio/vnd.wave",
+                               "_id": "id1",
+                               "metadata": {"tika_metadata_dcterms_created": datetime.utcnow().isoformat()}}
+                              ])
+        runner = CliRunner()
+        result = runner.invoke(cli, ['list-metadata', '--datashare-url', self.datashare_url, '--elasticsearch-url',
+                                     self.elasticsearch_url, '--datashare-project', self.datashare_project])
+        print(result)
+        json_result = loads(result.output)
+        self.assertEqual([{'count': 1, 'field': 'contentType', 'type': 'keyword'},
+                          {'count': 1, 'field': 'extractionDate', 'type': 'date'},
+                          {'count': 1,
+                           'field': 'metadata.tika_metadata_dcterms_created',
+                           'type': 'date'},
+                          {'count': 1, 'field': 'name', 'type': 'text'},
+                          {'count': 1, 'field': 'type', 'type': 'keyword'}], json_result)

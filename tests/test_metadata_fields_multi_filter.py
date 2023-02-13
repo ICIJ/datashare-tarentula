d744d356e6216a3586dcb1c2427721a2fb400612
Miguel <miguel.fiandor.gutierrez@gmail.com>
2023-02-13 12:54:06 +0100

    feat: add test with multiple filters"

---
+++ tests/test_metadata_fields_multi_filter.py
@@ -0,0 +1,41 @@
+from json import loads
+
+from click.testing import CliRunner
+
+from tarentula.cli import cli
+from .test_abstract import TestAbstract
+
+
+def load_json_file(path):
+    return loads(open(path).read())
+
+
+class TestMultiFilter(TestAbstract):
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_zero_results(self):
+        with self.existing_species_documents():
+            runner = CliRunner()
+            result = runner.invoke(cli, ['list-metadata', 
+                                        '--elasticsearch-url', self.elasticsearch_url, 
+                                        '--datashare-project', self.datashare_project,
+                                        '--filter_by', 'type=Document,contentType=message/abc', 
+                                        ])
+            json_result = loads(result.output)
+            self.assertEqual([], json_result)
+
+    def test_some_results(self):
+        with self.existing_species_documents():
+            runner = CliRunner()
+            result = runner.invoke(cli, ['list-metadata', 
+                                        '--elasticsearch-url', self.elasticsearch_url, 
+                                        '--datashare-project', self.datashare_project,
+                                        '--filter_by', 'type=Document,contentType=image/png', 
+                                        ])
+            json_result = loads(result.output)
+            self.assertEqual([{'count': 1, 'field': 'contentType', 'type': 'keyword'},
+                            {"count": 1, "field": "extractionDate", "type": "date"},
+                            {'count': 1, 'field': 'name', 'type': 'text'},
+                            {'count': 1, 'field': 'type', 'type': 'keyword'}], json_result)

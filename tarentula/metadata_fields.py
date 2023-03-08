d8e5b653207e569d3a7ec303d100866ce6f5e690
Bruno Thomas <bthomas@icij.org>
2023-03-08 19:25:01 +0000

    chore: lint metadata_fields.py

--- tarentula/metadata_fields.py
+++ tarentula/metadata_fields.py
@@ -1,3 +1,4 @@
+import sys
 from json import dumps
 
 from tarentula.datashare_client import DatashareClient, DATASHARE_DEFAULT_PROJECT, DATASHARE_DEFAULT_URL, \
@@ -42,43 +43,7 @@ class MetadataFields:
                                                     apikey)
         except (ConnectionRefusedError, ConnectionError):
             logger.critical('Unable to connect to Datashare', exc_info=self.traceback)
-            exit()
-
-    def query_mappings(self):
-        return self.datashare_client.mappings(self.datashare_project)
-
-    def query_field_count(self, complete_field_name):
-        query = {
-            "query": {"bool": {"must": {"match": {"type": self.type}},
-                               "filter": {"exists": {"field": complete_field_name}}}}
-        }
-        return self.datashare_client.count(self.datashare_project, query)
-
-    def get_fields(self, mapping, field_stack):
-        num_properties = len(mapping[self.datashare_project]['mappings']['properties'])
-        logger.info(f"We found {num_properties} properties indexed")
-
-        results = []
-        for field, properties in mapping[self.datashare_project]['mappings']['properties'].items():
-            complete_field_name = '.'.join(field_stack + [field])
-
-            if 'type' in properties:
-                item = {"field": complete_field_name, "type": properties["type"]}
-                if self.count:
-                    field_count = self.query_field_count(complete_field_name)
-                    if field_count["count"] > 0:
-                        item["count"] = field_count["count"]
-                        results.append(item)
-                else:
-                    results.append(item)
-
-            elif 'properties' in properties:
-                results += self.get_fields({self.datashare_project: {
-                    "mappings": mapping[self.datashare_project]['mappings']['properties'][field]}
-                },
-                    field_stack + [field])
-
-        return results
+            sys.exit()
 
     def query_mappings(self):
         return self.datashare_client.mappings(self.datashare_project)
@@ -102,7 +67,6 @@ class MetadataFields:
         return self.datashare_client.count(self.datashare_project, query)
 
     def get_fields(self, mapping, field_stack):
-
         results = []
         for field, properties in mapping[self.datashare_project]['mappings']['properties'].items():
             complete_field_name = '.'.join(field_stack + [field])

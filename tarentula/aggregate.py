a088dabc60855a9a94eaa5ce8ae6598ccbe364d4
Miguel <miguel.fiandor.gutierrez@gmail.com>
2023-03-10 16:38:11 +0100

    feat: tabular output option

--- tarentula/aggregate.py
+++ tarentula/aggregate.py
@@ -99,7 +99,6 @@ class AggCount(Aggregate):
     @staticmethod
     def tabular(agg):
         items = agg['aggregation-1']['buckets']
-
         # get all unique keys
         keys = []
         for item in items:
@@ -140,7 +139,7 @@ class DateHistogram(Aggregate):
     @staticmethod
     def tabular(agg):
         items = agg['aggregation-1']['buckets']
-
+        
         # get all unique keys
         keys = []
         for item in items:

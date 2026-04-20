d40d4a73a25728e53b3c2c42f61c632452f92929
pirhoo <hello@pirhoo.com>
2026-04-20 16:34:43 +0000

    perf(config): cache parsed ini contents

--- tarentula/config_file_reader.py
+++ tarentula/config_file_reader.py
@@ -1,3 +1,4 @@
+from functools import cached_property
 from typing import Optional
 from pathlib import Path
 from os.path import join, isfile
@@ -17,7 +18,7 @@ class ConfigFileReader:
             return self.config[self.section].get(self.name, self.default_value)
         return self.default_value
 
-    @property
+    @cached_property
     def config(self):
         config = configparser.ConfigParser()
         if self.config_path is not None:
